b8c09ad15f4b0185d6e4c5aa2d11e05b51c2bd82
linmaoxu <linmaoxu@4399inc.com>
2023-06-20 19:36:07 +0800

    <feature> add: OBB&OBB OBB&Sphere

--- Assets/Runtime/3DPhysics/Scripts/Entity/OBBEntity.cs
+++ Assets/Runtime/3DPhysics/Scripts/Entity/OBBEntity.cs
@@ -4,8 +4,33 @@ namespace Physics3D.Entity {
 
     public struct OBBEntity {
 
-        public Vector3 minPos;
-        public Vector3 maxPos;
+        public Vector3 pos;
         public Quaternion rot;
+        public Vector3[] vertexArray;
+        public Vector3 size;
+        public Vector3 halfSzie;
+        public Vector3 xAxis;
+        public Vector3 yAxis;
+        public Vector3 zAxis;
+        public void Init(Vector3 pos,Quaternion rot,Vector3 size,Vector3 xAxis,Vector3 yAxis,Vector3 zAxis) {
+            this.pos = pos;
+            this.rot = rot;
+            this.size = size;
+            halfSzie = size/2;
+            this.xAxis = xAxis;
+            this.yAxis = yAxis;
+            this.zAxis = zAxis;
+
+            vertexArray = new Vector3[8];
+            //定点计算有问题，没有添加旋转
+            vertexArray[0] = pos + rot*(new Vector3(halfSzie.x,halfSzie.y,halfSzie.z));
+            vertexArray[1] = pos + rot*(new Vector3(-halfSzie.x,halfSzie.y,halfSzie.z));
+            vertexArray[2] = pos + rot*(new Vector3(halfSzie.x,-halfSzie.y,halfSzie.z));
+            vertexArray[3] = pos + rot*(new Vector3(halfSzie.x,halfSzie.y,-halfSzie.z));
+            vertexArray[4] = pos + rot*(new Vector3(-halfSzie.x,-halfSzie.y,halfSzie.z));
+            vertexArray[5] = pos + rot*(new Vector3(-halfSzie.x,halfSzie.y,-halfSzie.z));
+            vertexArray[6] = pos + rot*(new Vector3(halfSzie.x,-halfSzie.y,-halfSzie.z));
+            vertexArray[7] = pos + rot*(new Vector3(-halfSzie.x,-halfSzie.y,-halfSzie.z));
+        }
     }
 }
--- Assets/Test/Scripts/Test3DIntersect.cs
+++ Assets/Test/Scripts/Test3DIntersect.cs
@@ -11,28 +11,28 @@ namespace Physics3D.Test
         public GameObject AABBGO2;
         public GameObject SphereGO1;
         public GameObject SphereGO2;
+        public GameObject OBBGO1;
+        public GameObject OBBGO2;
 
 
         AABBEntity aabb1;
         AABBEntity aabb2;
         SphereEntity sphere1;
         SphereEntity sphere2;
+        OBBEntity obb1;
+        OBBEntity obb2;
 
-        public void Awake()
+        public void Init()
         {
-
-        }
-
-        public void Init() {
             //Init
             aabb1 = new AABBEntity();
-            aabb1.size = new Vector3((float)AABBGO1.transform.localScale.x / 2, (float)AABBGO1.transform.localScale.y / 2,(float)AABBGO1.transform.localScale.z / 2);
+            aabb1.size = new Vector3((float)AABBGO1.transform.localScale.x / 2, (float)AABBGO1.transform.localScale.y / 2, (float)AABBGO1.transform.localScale.z / 2);
             aabb1.pos = AABBGO1.transform.position;
             aabb1.minPos = aabb1.pos - aabb1.size;
             aabb1.maxPos = aabb1.pos + aabb1.size;
 
             aabb2 = new AABBEntity();
-            aabb2.size = new Vector3((float)AABBGO2.transform.localScale.x / 2, (float)AABBGO2.transform.localScale.y / 2,(float)AABBGO2.transform.localScale.z / 2);
+            aabb2.size = new Vector3((float)AABBGO2.transform.localScale.x / 2, (float)AABBGO2.transform.localScale.y / 2, (float)AABBGO2.transform.localScale.z / 2);
             aabb2.pos = AABBGO2.transform.position;
             aabb2.minPos = aabb2.pos - aabb2.size;
             aabb2.maxPos = aabb2.pos + aabb2.size;
@@ -44,6 +44,12 @@ namespace Physics3D.Test
             sphere2 = new SphereEntity();
             sphere2.pos = SphereGO2.transform.position;
             sphere2.radius = (float)SphereGO2.transform.localScale.x;
+
+            obb1 = new OBBEntity();
+            obb1.Init(OBBGO1.transform.position, OBBGO1.transform.rotation, OBBGO1.transform.lossyScale, OBBGO1.transform.right, OBBGO1.transform.up, OBBGO1.transform.forward);
+
+            obb2 = new OBBEntity();
+            obb2.Init(OBBGO2.transform.position, OBBGO2.transform.rotation, OBBGO2.transform.lossyScale, OBBGO2.transform.right, OBBGO2.transform.up, OBBGO2.transform.forward);
         }
 
         public void OnDrawGizmos()
@@ -54,15 +60,20 @@ namespace Physics3D.Test
             Gizmos.DrawWireCube(AABBGO2.transform.position, AABBGO2.transform.localScale);
             Gizmos.DrawWireSphere(SphereGO1.transform.position, SphereGO1.transform.localScale.x);
             Gizmos.DrawWireSphere(SphereGO2.transform.position, SphereGO2.transform.localScale.x);
+            Gizmos.matrix = Matrix4x4.TRS(obb1.pos, obb1.rot, obb1.size);
+            Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+            Gizmos.matrix = Matrix4x4.TRS(obb2.pos, obb2.rot, obb2.size);
+            Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+            Gizmos.matrix = Matrix4x4.identity;
 
-            if (Intersect3DUtils.IntersectSphere_AABB( sphere1,aabb1))
+            if (Intersect3DUtils.IntersectSphere_AABB(sphere1, aabb1))
             {
                 Gizmos.color = Color.red;
                 Gizmos.DrawWireCube(AABBGO1.transform.position, AABBGO1.transform.localScale);
                 Gizmos.DrawWireSphere(SphereGO1.transform.position, SphereGO1.transform.localScale.x);
             }
 
-            if (Intersect3DUtils.IntersectSphere_AABB(sphere2,aabb1))
+            if (Intersect3DUtils.IntersectSphere_AABB(sphere2, aabb1))
             {
                 Gizmos.color = Color.red;
                 Gizmos.DrawWireCube(AABBGO1.transform.position, AABBGO1.transform.localScale);
@@ -81,6 +92,25 @@ namespace Physics3D.Test
                 Gizmos.color = Color.red;
                 Gizmos.DrawWireSphere(SphereGO1.transform.position, SphereGO1.transform.localScale.x);
                 Gizmos.DrawWireSphere(SphereGO2.transform.position, SphereGO2.transform.localScale.x);
+            }
+
+            if (Intersect3DUtils.IntersectOBB_OBB(obb1, obb2))
+            {
+                Gizmos.color = Color.red;
+                Gizmos.matrix = Matrix4x4.TRS(obb1.pos, obb1.rot, obb1.size);
+                Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+                Gizmos.matrix = Matrix4x4.TRS(obb2.pos, obb2.rot, obb2.size);
+                Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+                Gizmos.matrix = Matrix4x4.identity;
+            }
+
+            if (Intersect3DUtils.IntersectOBB_Sphere(obb1, sphere1))
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(SphereGO1.transform.position, SphereGO1.transform.localScale.x);
+                Gizmos.matrix = Matrix4x4.TRS(obb1.pos, obb1.rot, obb1.size);
+                Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+                Gizmos.matrix = Matrix4x4.identity;
             }
         }
 
